20977fbd9d05f253a6ef358ca45ec9bdba7a10b1
bananand <andrean.fuardi88@gmail.com>
2026-08-07 22:23:10 +0700

    feature: adding indonesian translations using omw

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -1,5 +1,10 @@
 import sqlite3
+import nltk
 from nltk.corpus import wordnet
+
+print("Memeriksa data WordNet multibahasa (OMW)...")
+nltk.download('omw-2.0', quiet=True)
+nltk.download('omw')
 
 def build_massive_database():
     conn = sqlite3.connect("src/kamus.db")
@@ -10,33 +15,40 @@ def build_massive_database():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             kata TEXT,
             definisi_en TEXT,
-            terjemahan_id TEXT
+            terjemahan TEXT,
+            UNIQUE(kata, definisi_en)
     )
-''')
+''') #UNIQUE(kata, definisi_en) berguna untuk membuat kombinasi unik 1 kosakata inggris dengan definisi miliknya sendiri
 
+    print("Mengekstrak kata. definisi, terjemahan..")
     print("Memulai pengambilan kata dari wordnet...")
     semua_kata = list(wordnet.words())
-    print(f"Berhasil mengambil {len(semua_kata)} kata dari WordNet.")
-
+    
     data_kata_siap_masuk = []
 
     for kata in semua_kata:
         synsets = wordnet.synsets(kata)
+        kata_terformat = kata.replace('_', ' ') #wordnet pakai underscore buat frasa
+
         for synset in synsets:
             definisi = synset.definition()
-            kata_terformat = kata.replace('_', ' ') #wordnet pakai underscore buat frasa
-            data_kata_siap_masuk.append((kata_terformat, definisi, ""))
+
+            daftar_indo = synset.lemma_names('ind')
+            #list comprehension python
+            terjemahan_bersih = ", ".join([indo.replace('_', ' ') for indo in daftar_indo])
+
+            data_kata_siap_masuk.append((kata_terformat, definisi, terjemahan_bersih))
     print(f"Memasukkan {len(data_kata_siap_masuk)} kata ke database.")     
 
     cursor.executemany('''
-        INSERT OR IGNORE INTO bank_kata_inggris (kata, definisi_en, terjemahan_id)
+        INSERT OR IGNORE INTO bank_kata_inggris (kata, definisi_en, terjemahan)
         VALUES (?, ?, ?)
     ''', data_kata_siap_masuk)
 
     conn.commit()
     conn.close()
 
-    print("SELESAI!, bank kosakata bahasa inggris telah terisi")
+    print("SELESAI!, bank kosakata bahasa inggris dan terjemahan indonesia telah terisi")
 
 if __name__ == "__main__":
     build_massive_database()
